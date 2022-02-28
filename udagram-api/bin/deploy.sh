e7a41297239d2206dd317e5dc72def117560c5cf
Ahmad Elshowair <it.is.work@outlook.com>
2022-02-28 22:52:48 +0700

    delete the version number

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -1,4 +1,4 @@
-eb init udagram-api --platform Node.js 14 --region us-east-1
+eb init udagram-api --platform Node.js --region us-east-1
 eb create --sample udagram-dev
 eb use udagram-dev --profile default
 eb deploy udagram-dev
